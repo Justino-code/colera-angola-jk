a618786e5ff3984c4eb846a0c76d2002cd6de1f6
Justino Contingo <jkotingo25@gmail.com>
2025-06-15 12:23:58 +0000

    organizando

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 3000,
+    allowedHosts: [
+      '*'
+      //'7495-102-218-85-39.ngrok-free.app'  // 🟢 adicione o domínio gerado pelo ngrok
+    ]
   }
 });
